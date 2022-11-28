80d5b44682590f2516a2e39e6bc8e589b5da2d7b
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2022-11-28 15:17:25 +0800

    Update Icons

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Header from '../components/Header'
@@ -6,8 +6,28 @@ import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
 import { WebView } from 'react-native-webview'
 import { View } from 'react-native'
+import axios from 'axios'
 
 export default function Dashboard({ navigation }) {
+
+  useEffect(() => {
+    const checkInterval = setInterval(() => {
+      axios.get('http://192.168.1.9:8000/func/auth/check-if-logged-in')
+      .then(function(res) {
+        const data = res.data;
+
+        if(data.logged_in == false) {
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'StartScreen' }]
+          });
+          clearInterval( checkInterval );
+        }
+
+      })
+    }, 1000)
+  }, [])
+
   return (
     <View style={{ flex: 1 }}>
       <WebView style={{ flex: 1 }} source={{ uri: 'http://192.168.1.9:8000/dashboard/staff/index' }} />
--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -4,8 +4,28 @@ import Logo from '../components/Logo'
 import Header from '../components/Header'
 import Button from '../components/Button'
 import Paragraph from '../components/Paragraph'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export default function StartScreen({ navigation }) {
+
+  useEffect(() => {
+    const checkInterval = setInterval(() => {
+      axios.get('http://192.168.1.9:8000/func/auth/check-if-logged-in')
+      .then(function(res) {
+        const data = res.data;
+
+        if(data.logged_in == true) {
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'Dashboard' }]
+          });
+        }
+
+      })
+    }, 1000)
+  }, [])
+
   return (
     <Background>
       <Logo />
